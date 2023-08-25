69f8ba75378d58d11832e9fd714a888d8b7181a4
Caner Aslan <canerdevx@gmail.com>
2023-08-25 18:13:21 +0300

    remove the problem description

--- the-kth-factor-of-n.cpp
+++ the-kth-factor-of-n.cpp
@@ -1,11 +1,6 @@
 // Author: Caner Aslan
 // Question Link: https://leetcode.com/problems/the-kth-factor-of-n/description/?envType=study-plan-v2&envId=amazon-spring-23-high-frequency
 
-/*
-    Problem Description:
-        You are given two positive integers n and k. A factor of an integer n is defined as an integer i where n % i == 0.          Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
-*/
-
 class Solution {
 public:
     int kthFactor(int n, int k) {
